1054bd996d2d3e53206f5ba0bca23fee9a3a2061
Mathieu Lecarme <mlecarme@bearstech.com>
2020-10-28 14:35:35 +0100

    Comments.

--- tests/test_sendmail.py
+++ tests/test_sendmail.py
@@ -1,15 +1,25 @@
 import os
 import uuid
 import time
+
 from imap_tools import MailBox, AND
 
 
 def test_sendmail(host):
+    """
+    Use `sendmail` command on the host to send a mail, and spot it with IMAP
+
+    Env:
+        MAIL_TARGET canary email
+        MAIL_SENDER from email
+        MAIL_IMAP IMAP server address
+        MAIL_PASSWORD password
+    """
     target = os.getenv("MAIL_TARGET")
     sender = os.getenv("MAIL_SENDER")
-    token = uuid.uuid4().hex
+    token = uuid.uuid4().hex  # link between emission and reception
 
-    t = host.run(
+    t = host.run( # shell command with `sendmail`
         (
             'echo "'
             "From: TestInfra <{sender}>\n"
@@ -24,13 +34,13 @@ def test_sendmail(host):
 
     with MailBox(os.getenv("MAIL_IMAP")).login(
         target, os.getenv("MAIL_PASSWORD")
-    ) as mailbox:
-        for i in range(10):
-            messages = list(
+    ) as mailbox:  # Connect and auth to IMAP
+        for i in range(10):  # Test 10 times
+            messages = list( # Search email from its subject
                 mailbox.fetch(AND(from_=sender, subject=token), headers_only=True)
             )
             print(messages)
             if len(messages) > 0:
-                return
-            time.sleep(1)
+                return  # Stop the loop, the test is ok
+            time.sleep(1) # Wait before retry
     assert False
