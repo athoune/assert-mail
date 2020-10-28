46298f49f8aa274ff8f22b6f345200f51bdfbf6f
Mathieu Lecarme <mlecarme@bearstech.com>
2020-10-28 16:51:07 +0100

    Better errors. Black.

--- tests/test_sendmail.py
+++ tests/test_sendmail.py
@@ -19,7 +19,7 @@ def test_sendmail(host):
     sender = os.getenv("MAIL_SENDER")
     token = uuid.uuid4().hex  # link between emission and reception
 
-    t = host.run( # shell command with `sendmail`
+    t = host.run(  # shell command with `sendmail`
         (
             'echo "'
             "From: TestInfra <{sender}>\n"
@@ -30,17 +30,20 @@ def test_sendmail(host):
         ).format(sender=sender, target=target, token=token)
     )
     print(t)
-    assert t.exit_status == 0
+    assert t.exit_status == 0, "sendmail command failed with %s" % t.stderr
 
-    with MailBox(os.getenv("MAIL_IMAP")).login(
-        target, os.getenv("MAIL_PASSWORD")
-    ) as mailbox:  # Connect and auth to IMAP
+    server = os.getenv("MAIL_IMAP")
+    passwd = os.getenv("MAIL_PASSWORD")
+    with MailBox(server).login(target, passwd) as mailbox:  # Connect and auth to IMAP
         for i in range(10):  # Test 10 times
-            messages = list( # Search email from its subject
+            messages = list(  # Search email from its subject
                 mailbox.fetch(AND(from_=sender, subject=token), headers_only=True)
             )
-            print(messages)
             if len(messages) > 0:
                 return  # Stop the loop, the test is ok
-            time.sleep(1) # Wait before retry
-    assert False
+            time.sleep(1)  # Wait before retry
+    assert (
+        False
+    ), "Mail {token} not found in IMAP account {target} of {server} in 10s".format(
+        token=token, target=target, server=server
+    )
