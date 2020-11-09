99a8255d5cd039a9a3d520a6c72d2d2df0d9cce4
Mathieu Lecarme <mlecarme@bearstech.com>
2020-11-09 15:25:22 +0100

    More comments.

--- tests/test_postfix.py
+++ tests/test_postfix.py
@@ -7,14 +7,17 @@ SPACES = re.compile(r"^\s+")
 
 
 def postfix_reader(iterator):
+    "Read postfix config lines, and yield key, value."
     current = StringIO()
     for line in iterator:
         if line.strip() == "" or line.startswith("#"):
+            # empty line or line starting with #
             continue
         if not SPACES.search(line) and current.tell():
+            # line doesn't start with space, and current buffer is not empty
             current.seek(0)
             yield tuple(EQUAL.split(current.read(), 1))
-            current = StringIO()
+            current = StringIO()  # reset the buffer
         current.write(line.lstrip())
     if current.tell():
         current.seek(0)
@@ -33,9 +36,11 @@ def test_postfix(host):
     assert main.exists
     for key, value in postfix_reader(main.content_string.split("\n")):
         if value.startswith("hash:"):
+            # assert that toto exists with hash:/toto
             path = value.split(":", 1)[1]
             assert host.file(path).exists, "%s doesn't exist" % path
             assert host.file("%s.db" % path).exists, "%s.db doesn't exist" % path
         if value.startswith("regexp:"):
+            # assert that toto exists with regexp:/toto
             path = value.split(":", 1)[1]
             assert host.file(path).exists, "%s doesn't exist" % path
